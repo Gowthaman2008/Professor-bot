46b82e343ee20ada7884ceb430b7d7296d3e63b2
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-06-25 13:26:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,7 +65,7 @@ SHORT_API = environ.get("SHORT_API")
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "600"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '@movies_tamizhaass')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movies_tamizhaass')
 P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
 IMDB = is_enabled(environ.get('IMDB', "True"), True)
