15a4ad19680f6dea8337948765d769be13c51b01
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-06-28 19:38:27 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -276,7 +276,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"🎪 Here is what i found for your query {search} \n ┏ 👦🏼 ᴀsᴋᴇᴅ ʙʏ - {user}"
+        cap = f"🎪 Here is what i found for your query {search}"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
