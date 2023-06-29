5e226f16a3970a64e223b95d6f3afacc9b6eaaa1
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-06-29 17:07:56 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -276,7 +276,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"**📥 Your Movie File Ready ✅ `{search}`**"
+        cap = f"**📥 Your Movie File Ready ✅ `{search}`👇**"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
