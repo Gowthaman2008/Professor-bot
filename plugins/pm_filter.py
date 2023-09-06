07317c19098c93f649e6d0e7e066152d3bc2f8f3
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-09-06 08:38:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -59,11 +59,7 @@ async def pm_next_page(bot, query):
         else:
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'pmfile#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
-            
-            btn.insert(0, [
-        InlineKeyboardButton("🍁 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍁", url=f"https://t.me/HTD_Tn_links/21")
-    ])
-
+        
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
@@ -71,22 +67,27 @@ async def pm_next_page(bot, query):
     else:
         off_set = offset - 10
     if n_offset == 0:
-        btn.append(
-            [InlineKeyboardButton("⏪ Back", callback_data=f"pmnext_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"◽Pages {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="page")]
-        )
+        btn.append([
+            InlineKeyboardButton("🍁 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍁", url=f"https://t.me/HTD_Tn_links/21")
+            ],[
+             InlineKeyboardButton("⏪ Back", callback_data=f"pmnext_{req}_{key}_{off_set}"),
+             InlineKeyboardButton(f"◽Pages {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="page")
+        ])
     elif off_set is None:
-        btn.append(
-            [InlineKeyboardButton(f"◽ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-             InlineKeyboardButton("Next ⏩", callback_data=f"pmnext_{req}_{key}_{n_offset}")])
+        btn.append([
+             InlineKeyboardButton("🍁 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍁", url=f"https://t.me/HTD_Tn_links/21")
+             ],[
+             InlineKeyboardButton(f"◽ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
+             InlineKeyboardButton("Next ⏩", callback_data=f"pmnext_{req}_{key}_{n_offset}")
+        ])
     else:
-        btn.append(
-            [
+        btn.append([ 
+                InlineKeyboardButton("🍁 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍁", url=f"https://t.me/HTD_Tn_links/21")
+                ],[
                 InlineKeyboardButton("⏪ Back", callback_data=f"pmnext_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"◽ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
                 InlineKeyboardButton("Next ⏩", callback_data=f"pmnext_{req}_{key}_{n_offset}")
-            ],
-        )
+            ],)
     try:
         await query.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(btn)
@@ -151,10 +152,11 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
         temp.PM_BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
+            InlineKeyboardButton("🍁 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍁", url=f"https://t.me/HTD_Tn_links/21")
+            ],[
             [InlineKeyboardButton(text=f"◽Page 1/{math.ceil(int(total_results) / 6)}", callback_data="pages"),
             InlineKeyboardButton(text="Next ⏩", callback_data=f"pmnext_{req}_{key}_{offset}")
-            ]
-        )
+            ])
     else:
         btn.append(
             [InlineKeyboardButton(text="◽Page 1/1", callback_data="pages")]
