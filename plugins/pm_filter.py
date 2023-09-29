2defcbde33e176853303fe5736cffc41082ad71e
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-09-29 16:04:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -59,7 +59,11 @@ async def pm_next_page(bot, query):
         else:
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'pmfile#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
-        
+
+        #btn.insert(0, [
+             #InlineKeyboardButton("🍁 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍁", url=f"https://t.me/HTD_Tn_links/21")
+        ])
+
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
